0158379d5caf5458c1e6df5e9de1e3be20a986ab
jeremy klassen <jeremyklassen@jeremys-MacBook-Air.local>
2022-12-04 18:14:06 -0600

    Refactored DB.py code. Moved boilerplate code to the constructor. Also modified the README.md

--- Model/DB/DB.py
+++ Model/DB/DB.py
@@ -5,96 +5,69 @@ from Model.DB import Constants as C
 class DB():
     def __init__(self):
         self.C = C.Constants()
+        self.conn = sqlite3.connect(self.C.DBPATH)
+        self.c = self.conn.cursor()
 
 # Getters and Setters
     def getAPIKeys(self):
-        conn = sqlite3.connect(self.C.DBPATH)
-        c = conn.cursor()
-        c.execute('SELECT * FROM keys;')
-        newKeys = c.fetchall()
+        self.c.execute('SELECT * FROM keys;')
+        newKeys = self.c.fetchall()
         return newKeys
 
     def getUser(self,table,id):
         sql = "SELECT * FROM " + table + " WHERE id = " + id
-        conn = sqlite3.connect(self.C.DBPATH)
-        c = conn.cursor()
-        c.execute(sql)
-        ret = c.fetchall()
-        conn.close()
+        self.c.execute(sql)
+        ret = self.c.fetchall()
         return ret
 
 # Functionality Methods
     def addUsers(self, clients, drivers):
-        conn = sqlite3.connect(self.C.DBPATH)
-        c = conn.cursor()
-
         if clients:
-            c.execute('INSERT INTO clients VALUES (?,?,?,?,?,?)', clients)
+            self.c.execute('INSERT INTO clients VALUES (?,?,?,?,?,?)', clients)
         if drivers:
-            c.execute('INSERT INTO drivers VALUES (?,?,?,?,?,?)', drivers)
+            self.c.execute('INSERT INTO drivers VALUES (?,?,?,?,?,?)', drivers)
 
-        conn.commit()
-        conn.close()
+        self.conn.commit()
 
     def createDB(self):
-        myFile = Path(self.C.DBPATH)
-        if myFile.is_file():
-            print("Client DB Already exists.")
-            return
-        
-        conn = sqlite3.connect(self.C.DBPATH)
-        c = conn.cursor()
-        c.execute(''' CREATE TABLE clients
-            (id INTEGER PRIMARY KEY AUTOINCREMENT, lName TEXT, fName TEXT, pc TEXT, lat TEXT, long TEXT)''')
 
-        c.execute(''' CREATE TABLE drivers
+        self.c.execute(''' CREATE TABLE IF NOT EXISTS clients
             (id INTEGER PRIMARY KEY AUTOINCREMENT, lName TEXT, fName TEXT, pc TEXT, lat TEXT, long TEXT)''')
-
-        c.execute(''' CREATE TABLE keys
+        self.c.execute(''' CREATE TABLE IF NOT EXISTS drivers
+            (id INTEGER PRIMARY KEY AUTOINCREMENT, lName TEXT, fName TEXT, pc TEXT, lat TEXT, long TEXT)''')
+        self.c.execute(''' CREATE TABLE IF NOT EXISTS keys
             (keyType text, key text)''')
 
-        conn.commit()
-        conn.close()
+        self.conn.commit()
 
     def updateUser(self,update, table, id):
         sql = "UPDATE " + table + " SET lat = " + update[0] + ", long = " + update[1] + "WHERE id = " + id
-        conn = sqlite3.connect(self.C.DBPATH)
-        c = conn.cursor()
-        c.execute(sql)
-        conn.commit()
-        conn.close()
+        self.c.execute(sql)
+        self.conn.commit()
 
     def changeKey(self,keys):
-        print("Test")
-        print(self.C.DBPATH)
-        conn = sqlite3.connect(self.C.DBPATH)
-        c = conn.cursor()
 
         if keys[0]:
-            c.execute(''' DELETE FROM keys
+            self.c.execute(''' DELETE FROM keys
                 WHERE keyType = 'API_ID'; ''')
-            c.execute('INSERT INTO keys VALUES (?,?)', [keys[0],keys[1]])
+            self.c.execute('INSERT INTO keys VALUES (?,?)', [keys[0],keys[1]])
             self.C.API_ID = keys[0]
-
         if keys[1]:
-            c.execute(''' DELETE FROM keys
+            self.c.execute(''' DELETE FROM keys
                 WHERE keyType = 'API_KEY'; ''')
-            c.execute('INSERT INTO keys VALUES (?,?)', [keys[2],keys[3]])
+            self.c.execute('INSERT INTO keys VALUES (?,?)', [keys[2],keys[3]])
             self.C.API_ID = keys[1]
-
         if keys[2]:
-            c.execute(''' DELETE FROM keys
+            self.c.execute(''' DELETE FROM keys
                 WHERE keyType = 'PC_KEY'; ''')
-            c.execute('INSERT INTO keys VALUES (?,?)', [keys[4],keys[5]])
+            self.c.execute('INSERT INTO keys VALUES (?,?)', [keys[4],keys[5]])
             self.C.PC_Key = keys[2]
 
-        conn.commit()
-        conn.close()
+        self.conn.commit()
 
 if __name__ == '__main__':
     db = DB()
     test = db.getAPIKeys
-    print(test)
     db.createDB()
     client = ['Teddy','Roosevelt','R2K']
     driver = ['Joe','Biden','R3L']
--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Drive Scheduler
 ## Jeremy Klassen
 ---
-Drive Scheduler is a python project in development. It takes in a list of drivers locations, and a list of clients locations and then calculates the optimal pickup pairings based on total distance or time.
+Drive Scheduler is a python project in development. It takes in a list of driver locations, and a list of client locations. It then calculates the optimal pickup pairings based on total distance or time.
 ---
 ### Minimum Viable Product features
 The minimum features this project will include are:
 
-- A SQLite database that you create and input data from the script. It has 3 tables (clients, drivers, and API keys).
-- The script will consume 2 APIs one that will convert an address string into GPS coordinates, and one that will return drive distances between each driver coordinates and the client coordinates (one to many).
-- Calculate and output a list of Driver/Client pairings that collectively require the least amount of drive distance.
+- A SQLite database that users can input data to, from the script. It has 3 tables (clients, drivers, and API keys).
+- The script will consume 2 APIs. One that will convert an address/location string into GPS coordinates, and one that will return drive distances between each driver coordinates (one) and all the listed clients' coordinates (many).
+- Calculate and output a list of Driver/Client pairings that collectively require the least amount of drive distance for each client to be picked up.
 
 ---
 ### Potential Additional Features (in order of need)
-- optionally outputs a list based on drive time instead of drive distance.
+- Optionally outputs a list based on drive time instead of drive distance.
 - GUI for Pythonista (IOS). So that the script can be optimally run from a cellphone.
 - Read data into database from .csv or spreadsheet files.
 - Ability to set specific driver/client pairs before calculating a schedule.
